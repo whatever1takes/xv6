a3be43e28cedea5fea3ba05a84de59c56009a3ff
whatever1takes <qq763421316@163.com>
2025-04-06 23:29:38 +0800

    update sleep.c

--- user/sleep.c
+++ user/sleep.c
@@ -6,15 +6,15 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-//  if (argc != 2) {
-//    printf("Error. Enter for example: sleep 2\n");
-//    exit(-1);
-//  }
-//   int sleep_time = atoi(argv[1]);
-//   if (sleep_time < 0) {
-//     printf("Invalid sleep time\n");
-//     exit(-1);
-//   }
-  sleep(atoi(argv[1]));
+  if (argc != 2) {
+    printf("Error. Enter for example: sleep 2\n");
+    exit(-1);
+  }
+   int sleep_time = atoi(argv[1]);
+   if (sleep_time < 0) {
+     printf("Invalid sleep time\n");
+     exit(-1);
+   }
+  sleep(sleep_time);
   exit(0);
 }
