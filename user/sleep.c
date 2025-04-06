5f0d219aa8a20a757dde9b2616c8a209c66951ff
whatever1takes <qq763421316@163.com>
2025-04-06 22:50:37 +0800

    update sleep.c

---
+++ user/sleep.c
@@ -0,0 +1,14 @@
+//
+// Created by 周文浩 on 25-4-6.
+//
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "../user/user.h"
+
+int main(int argc, char *argv[]){
+    if(argc<=1){
+        exit(0);
+    }
+    sleep(atoi(argv[0]));
+    exit(0);
+}
