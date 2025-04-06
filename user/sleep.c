f3088bc3ff87198cdf924bedd84819e91dd45c6f
whatever1takes <qq763421316@163.com>
2025-04-06 23:23:40 +0800

    update sleep.c

--- user/sleep.c
+++ user/sleep.c
@@ -1,14 +1,20 @@
 //
 // Created by 周文浩 on 25-4-6.
 //
-#include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/types.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]){
-    if(argc<=1){
-        exit(0);
-    }
-    sleep(atoi(argv[0]));
-    exit(0);
+int main(int argc, char *argv[]) {
+//  if (argc != 2) {
+//    printf("Error. Enter for example: sleep 2\n");
+//    exit(-1);
+//  }
+  int sleep_time = atoi(argv[1]);
+  if (sleep_time < 0) {
+    printf("Invalid sleep time\n");
+    exit(-1);
+  }
+  sleep(sleep_time);
+  exit(0);
 }
